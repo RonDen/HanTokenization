d9f4761296cca445778e9e7b8308136011aa210a
RonDen <3391131052@qq.com>
2021-04-07 22:21:43 +0800

    最大后向匹配算法

---
+++ notebooks/max-ahead-back.py
@@ -0,0 +1,50 @@
+import os
+
+DATAROOT = '../datasets'
+RESULTROOT = '../results'
+VOCAB_FILE = os.path.join(DATAROOT, 'training_vocab.txt')
+TRAIN_FILE = os.path.join(DATAROOT, 'training.txt')
+TEST_FILE = os.path.join(DATAROOT, 'test.txt')
+
+vocab = set()
+train_set = []
+test_set = []
+
+with open(VOCAB_FILE, 'r', encoding='utf-8') as f:
+    vocab = set(map(str.strip, f.readlines()))
+
+with open(TRAIN_FILE, 'r', encoding='utf-8') as f:
+    train_set = list(map(str.strip, f.readlines()))
+
+with open(TEST_FILE, 'r', encoding='utf-8') as f:
+    test_set = list(map(str.strip, f.readlines()))
+
+
+
+train_set_split = [line.split('  ') for line in train_set]
+test_set_split = [line.split('  ') for line in test_set]
+
+train_raw = [''.join(line) for line in train_set_split]
+test_raw = [''.join(line) for line in test_set_split]
+
+
+def max_back(line: str, max_len = 4):
+    res = []
+    n = len(line)
+    idx = 0
+    while idx < n:
+        lens = max_len
+        while lens > 0:
+            sub = line[idx: idx + lens]
+            # print(sub)
+            if sub in vocab:
+                res.append(sub)
+                idx += lens
+                lens = max_len
+            else:
+                lens -= 1
+    return res
+
+with open(os.path.join(RESULTROOT, 'max-back-result.txt'), 'w+', encoding='utf-8') as f:
+    f.write('\n'.join([max_back(line) for line in test_raw]))
+
