ebb9b1fb15d1bc4f1db12ca1fd708c6a40c02ef3
LuoD <3391131052@qq.com>
2021-04-08 10:02:45 +0800

    测试结果到logs目录

--- runeval.sh
+++ runeval.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
-perl scripts/score datasets/training_vocab.txt datasets/test.txt results/max-back-result.txt
+echo "max-back, max_len = 3"
+perl scripts/score datasets/training_vocab.txt datasets/test.txt results/max-back-result-3.txt
+echo "max-back, max_len = 4"
+perl scripts/score datasets/training_vocab.txt datasets/test.txt results/max-back-result-4.txt
+echo "max-back, max_len = 5"
+perl scripts/score datasets/training_vocab.txt datasets/test.txt results/max-back-result-5.txt
+
+echo "max-forward, max_len = 3"
+perl scripts/score datasets/training_vocab.txt datasets/test.txt results/max-forward-result-3.txt
+echo "max-forward, max_len = 4"
+perl scripts/score datasets/training_vocab.txt datasets/test.txt results/max-forward-result-4.txt
+echo "max-forward, max_len = 5"
+perl scripts/score datasets/training_vocab.txt datasets/test.txt results/max-forward-result-5.txt
